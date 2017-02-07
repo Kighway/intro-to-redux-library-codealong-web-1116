843ee11196ddec7a2b7da5ab99ba0e6832723efa
Kyle Tulod <tulod.kyle.m@gmail.com>
2017-02-07 17:18:54 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -12,3 +12,21 @@ class App extends Component {
 }
 
 export default App;
+
+/*
+We can try to see changes in our state if we add the following code in: src/App.js:. (It won't work, but let's give it a shot - A for effort I always say.)
+
+  class App extends Component {
+    handleOnClick(){
+      this.props.store.dispatch({type: 'INCREASE_COUNT'})
+    }
+    render() {
+      return (
+        <div className="App">
+            <button onClick={this.handleOnClick.bind(this)}>Click</button>
+            <p> {this.props.store.getState().items.length}</p>
+        </div>
+      );
+    }
+  }
+*/
--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,9 +1,25 @@
+/*
 export default function shoppingListItemReducer(state = {items: []}, action) {
   console.log(action)
   switch(action.type) {
     case 'GET_COUNT_OF_ITEMS':
-    let items;
-    items = state.items.concat(state.items.length + 1)
+    let items; // <-------------------------------------------------------------
+    items = state.items.concat(state.items.length + 1)// <----------------------
+    let proposedstate = Object.assign({}, {items: items})
+    console.log(proposedstate)
+     return proposedstate;
+    default:
+      console.log(state)
+      return state;
+  }
+}
+*/
+
+export default function shoppingListItemReducer(state = {items: []}, action) {
+  console.log(action)
+  switch(action.type) {
+    case 'GET_COUNT_OF_ITEMS':
+    let items = state.items.concat(state.items.length + 1)
     let proposedstate = Object.assign({}, {items: items})
     console.log(proposedstate)
      return proposedstate;
